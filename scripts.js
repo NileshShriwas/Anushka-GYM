4520df585851110e5e223c250872ef29cdc2df6b
Nilesh Shriwas <nileshshriwas57@gmail.com>
2021-09-05 02:07:46 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Create function to select element
+const selectElement = (element) => document.querySelector(element);
+
+// Open and Close nav on click
+selectElement('.menu-icons').addEventListener('click', () => {
+    selectElement('nav').classList.toggle('active');
+});
